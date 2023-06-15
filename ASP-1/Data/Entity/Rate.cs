6cad93b1397f05944cafe2082fceecc4bd1b87e6
Pavel Lupashko <pavel.lupashko228@gmail.com>
2023-06-15 20:13:08 +0300

    вроде норм

--- ASP-1/Data/Entity/Rate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASP_1.Data.Entity
-{
-    public class Rate
-    {
-        public Guid ItemId { get; set; }
-        public Guid UserId { get; set; }
-        public int Rating { get; set; } = 0;
-    }
-}
